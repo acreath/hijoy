8ce87ceb6da76f58f4f887246625fb76b3546021
郑天yi <zhengtianyi@aidigger.com>
2019-10-31 13:47:59 +0800

    基础介绍的实现

--- flasky/index.py
+++ flasky/index.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask import request,make_response,redirect,abort
+
 
 
 
@@ -6,13 +8,35 @@ app = Flask(__name__)
 
 @app.route('/') 
 def index():
-    return '<h1>Hello World!</h1>'
+    
+    user_agent = request.headers.get('User-Agent')
+    print("information of request:{}".format(request.headers))
+    return '<p>Your browser is %s</p>' %user_agent
+    #return '<h1>Hello World!</h1>'
 
 
 @app.route('/user/<name>') 
 def user(name):
     return '<h1>Hello, %s!</h1>' % name
 
+@app.route('/reponse')
+def reponses():
+    response = make_response('<h1>This document carries a cookie</h1>')
+    response.set_cookie('answer','42')
+    
+    return response
+
+@app.route('/404')
+def red():
+    return redirect('http://www.example.com')
+
+
+@app.route('/user/<id>') 
+def get_user(id):
+    user = load_user(id) 
+    if not user:
+        abort(404)
+    return '<h1>Hello, %s</h1>' % user.name
 
 if __name__ == '__main__': 
     app.run(debug=True)
