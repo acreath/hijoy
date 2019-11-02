13598009da2e80ebf253461cc77e6f8d08b0e68d
郑天yi <zhengtianyi@aidigger.com>
2019-11-02 21:08:02 +0800

    完成一些基础学习

--- flasky/index.py
+++ flasky/index.py
@@ -1,23 +1,26 @@
-from flask import Flask
+from flask import Flask,render_template
 from flask import request,make_response,redirect,abort
-
-
-
+from flask_script import Manager
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+manager = Manager(app)
+bootstrap = Bootstrap(app)
+
 
 @app.route('/') 
 def index():
-    
+    """
     user_agent = request.headers.get('User-Agent')
     print("information of request:{}".format(request.headers))
     return '<p>Your browser is %s</p>' %user_agent
     #return '<h1>Hello World!</h1>'
-
+    """
+    return render_template('index.html')
 
 @app.route('/user/<name>') 
 def user(name):
-    return '<h1>Hello, %s!</h1>' % name
+    return render_template('user.html',name = name)
 
 @app.route('/reponse')
 def reponses():
@@ -28,7 +31,7 @@ def reponses():
 
 @app.route('/404')
 def red():
-    return redirect('http://www.example.com')
+    return redirect('http://www.baidu.com')
 
 
 @app.route('/user/<id>') 
@@ -38,5 +41,15 @@ def get_user(id):
         abort(404)
     return '<h1>Hello, %s</h1>' % user.name
 
+
+@app.errorhandler(404) 
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
 if __name__ == '__main__': 
-    app.run(debug=True)
+    #app.run(debug=True)
+    manager.run()
