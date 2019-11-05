5028530f1e983402b54699bba52cb7dde4da42de
郑天yi <zhengtianyi@aidigger.com>
2019-11-05 23:20:36 +0800

    构建项目结构和配置文件

---
+++ flasky/config.py
@@ -0,0 +1,40 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))#应该是为了生成本地的数据库
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    SQLALCHEMY_TRACK_MODIFICATIONS =True
+    FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky-注册认证]'
+    FLASKY_MAIL_SENDER = '2521439785@qq.com' 
+    FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
+
+    @staticmethod
+    def init_app(app):
+        pass#这个作用需要再看看
+class DevelopmentConfig(Config): 
+    DEBUG = True
+    MAIL_SERVER = 'smtp.qq.com'
+    MAIL_PORT = 465
+    MAIL_USE_SSL = True
+    MAIL_USE_TLS = False
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') 
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
+        'mysql+pymysql://root:password@localhost:3306/flasky'
+
+class TestingConfig(Config): 
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \ 
+        'mysql+pymysql://root:password@localhost:3306/flasky'
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'mysql+pymysql://root:password@localhost:3306/flasky'
+
+config = {
+    'development': DevelopmentConfig, 
+    'testing': TestingConfig, 
+    'production': ProductionConfig,
+    'default': DevelopmentConfig 
+}
