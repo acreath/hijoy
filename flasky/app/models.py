37ef4b198dadabb8b5f029661115dec8d4cb8eea
郑天yi <zhengtianyi@aidigger.com>
2019-11-09 18:50:09 +0800

    实现登录部分

--- flasky/app/models.py
+++ flasky/app/models.py
@@ -1,4 +1,9 @@
 from . import db
+import pylint_flask
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin, login_required
+from . import login_manager
+
 
 class Role(db.Model):
     __tablename__ = 'roles'
@@ -14,6 +19,25 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    password_hash = db.Column(db.String(128))
+    email = db.Column(db.String(64), unique=True, index=True)
+
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+    
+    #计算密码散列值的函数通过名为 password 的只写属性实现
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+    
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
         return '<User %r>' % self.username
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
