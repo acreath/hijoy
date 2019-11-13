219312bbe9747c8f90415a97de22d481db07b7bf
郑天yi <zhengtianyi@aidigger.com>
2019-11-13 23:58:24 +0800

    user register

--- flasky/app/auth/views.py
+++ flasky/app/auth/views.py
@@ -2,8 +2,8 @@ from flask import render_template, redirect, request, url_for, flash
 from . import auth
 from flask_login import login_user, login_required, logout_user, UserMixin
 from ..models import User
-from .forms import LoginForm
-import app
+from .forms import LoginForm, RegistrationForm
+from .. import db
 
 #登入用户的逻辑
 @auth.route('/login', methods=['GET', 'POST'])
@@ -25,6 +25,20 @@ def logout():
     logout_user()
     flash('You have been logged out.')
     return redirect(url_for('main.index'))
+
+
+    
+#用户注册逻辑
+@auth.route('/register', methods=['GET','POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data, username=form.username.data, password=form.password.data)
+        db.session.submit(user)
+        flash('You can now login')
+        return redirect(url_for('auth.login'))
+    return render_template('auth/login.html', form=form)
+
 
 @auth.route('/secret')
 @login_required
