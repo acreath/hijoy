ef8a7e08f7e05d0dd5fefd8d73195c0d6ab86738
郑天yi <zhengtianyi@aidigger.com>
2019-11-20 23:13:30 +0800

    增加角色与权限

--- flasky/app/models.py
+++ flasky/app/models.py
@@ -5,16 +5,69 @@ from flask_login import UserMixin, login_required
 from . import login_manager
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer 
 from flask import current_app
+'''
+程序的权限
 
+操  作  |  位  值  |  说  明
+关注用户 | 0b00000001(0x01) |关注其他用户
+在他人的文章中发表评论 | 0b00000010(0x02) | 在他人撰写的文章中发布评论
+写文章 |0b00000100(0x04) | 写原创文章
+管理他人发表的评论 | 0b00001000(0x08) | 查处他人发表的不当评论
+管理员权限 | 0b10000000(0x80) |管理网站
+     
+'''
+
+'''
+用户角色
+
+用户角色  |  权限  |  说明
+匿名 | 0b00000000(0x00) | 未登录的用户。在程序中只有阅读权限
+用户 | 0b00000111(0x07) | 具有发布文章、发表评论和关注其他用户的权限。这是新用户的默认角色  
+协管员 | 0b00001111(0x0f) | 增加审查不当评论的权限
+管理员 | 0b11111111(0xff) | 具有所有权限，包括修改其他用户所属角色的权限
+'''
+#权限常量
+class Permission:
+    FOLLOW = 0x01
+    COMMENT = 0x02
+    WRITE_ARTICLES = 0x04
+    MODERATE_COMMENTS = 0x08
+    ADMINISTER = 0x80
 
 class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
-    users = db.relationship('User', backref='role')
+    default = db.Column(db.Boolean,default=False, index=True)
+    permissions = db.Column(db.Integer)
+    users = db.relationship('User', backref='role',lazy='dynamic')
+
 
     def __repr__(self):
         return '<Role %r>' % self.name
+
+    #使用这个方法在数据中创建角色
+    @staticmethod
+    def insert_roles():
+        #set the permissions of the different roles
+        roles = {
+            'User': (Permission.FOLLOW | 
+                    Permission.COMMENT | 
+                    Permission.WRITE_ARTICLES, True),
+            'Moderator': (Permission.FOLLOW | 
+                          Permission.COMMENT | 
+                          Permission.WRITE_ARTICLES |
+                          Permission.MODERATE_COMMENTS, False),
+            'Administrator': (0xff, False)
+        }
+        for r in roles:
+            role = Role.query.filter_by(name = r).first()
+            if role is None:
+                role = Role(name=r)
+            role.permissions = roles[r][0]
+            role.default = roles[r][1]
+            db.session.add(role)
+        db.session.commit()
 
 class User(db.Model, UserMixin):
     __tablename__ = 'users'
